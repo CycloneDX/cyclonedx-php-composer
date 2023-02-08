2247f37db80595e6888c866bea63746c1865e85f
Jan Kowalleck <jan.kowalleck@gmail.com>
2023-02-08 10:28:12 +0100

    demos change `-d`
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- demo/create-sbom-results.sh
+++ demo/create-sbom-results.sh
@@ -7,5 +7,5 @@ COMPOSER_BIN="${COMPOSER_BIN:-$(which composer)}"
 
 find "$THIS_DIR" -mindepth 2 -maxdepth 2 -type d -name 'project' \
 -print \
--exec "$COMPOSER_BIN" -d'{}' setup \; \
--exec "$COMPOSER_BIN" -d'{}' create-sbom-results -vv \;
+-exec "$COMPOSER_BIN" -d '{}' setup \; \
+-exec "$COMPOSER_BIN" -d '{}' create-sbom-results -vv \;
